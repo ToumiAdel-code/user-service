f16fdc70edb0bb09665165e3e3e49661e267fbf9
Adeltoumi-Code <adel.toumi@esprit.tn>
2026-09-10 15:17:30 +0100

    feat: document create user endpoint

--- src/main/java/user_service/controller/UserController.java
+++ src/main/java/user_service/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     public List<User> getUsers() {
         return service.getAllUsers();
     }
-
+    // Create a new user
     @PostMapping
     public User createUser(@RequestBody User user) {
         return service.createUser(user);
